053a2426502a8e401cbc33ad58158437ce472adf
Senthilrajan-sundaram <senthilrajan.sundaram@outlook.com>
2024-10-26 13:48:21 +0530

    added-keyvault

---
+++ keyvault/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "this" {
+  
+  name                            = var.settings.azurerm_key_vault_name
+  location                        = var.settings.location
+  resource_group_name             = var.settings.resource_group_name
+  enabled_for_disk_encryption     = var.settings.enabled_for_disk_encryption
+  tenant_id                       = var.settings.tenant_id
+  soft_delete_retention_days      = var.settings.soft_delete_retention_days
+  purge_protection_enabled        = var.settings.purge_protection_enabled
+  tags                            = var.settings.tags
+  sku_name                        = var.settings.sku_name
+  enabled_for_template_deployment = var.settings.enabled_for_template_deployment
+  enabled_for_deployment          = var.settings.enabled_for_deployment
+  enable_rbac_authorization       = var.settings.enable_rbac_authorization
+  public_network_access_enabled   = var.settings.public_network_access_enabled
+}
---
+++ keyvault/variable.tf
@@ -0,0 +1,19 @@
+variable "settings" {
+  description = "Key vault configuration."
+  type = object({
+    azurerm_key_vault_name          = string
+    location                        = string
+    resource_group_name             = string
+    sku_name                        = optional(string, "premium")
+    purge_protection_enabled        = optional(bool, false)
+    tags                            = optional(map(string), null)
+    enabled_for_disk_encryption     = optional(bool, true)
+    tenant_id                       = string
+    soft_delete_retention_days      = optional(number)
+    enabled_for_template_deployment = optional(bool)
+    enabled_for_deployment          = optional(bool)
+    enable_rbac_authorization       = optional(bool)
+    public_network_access_enabled   = optional(bool)
+    secret_expiration_date          = optional(string)
+  })
+}
